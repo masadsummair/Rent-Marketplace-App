65cf02ec0d88f47c955acdfe252713a371485d6a
Muhammad Asad Summair <62187782+masads@users.noreply.github.com>
2021-10-21 15:26:39 +0500

    database created

--- server/server.js
+++ server/server.js
@@ -4,11 +4,19 @@ const app = express();
 const port = 3000;
 var db = require('./utils/database.js');
 
-db.connect(function(err, connection) {
-  let res=db.query("drop DATABASE nodetest");
-  // console.log(res);
+
+db.connect(function(err) {
+  db.query("select * from testnode;" , function(err,result,fields)
+  {
+    setValue(result);
+  });
 });
 
+console.log(data);
+function setValue(res)
+{
+  data=Object.values(JSON.parse(JSON.stringify(res)));
+}
 
 
 app.get('/', (req, res) => {
--- server/utils/database.js
+++ server/utils/database.js
@@ -3,7 +3,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:''
+    password:'',
+    database:'test'
 });
 
 db.connect(err=>
