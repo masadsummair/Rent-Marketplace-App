ef1c0659f654a70b634804282a4aa91e9d4d9a78
Muhammad Asad Summair <62187782+masads@users.noreply.github.com>
2021-10-21 15:37:47 +0500

    client update

--- client/App.js
+++ client/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
