4a5fa69edadaa3e5d1f1a6ceb98fb7c9a5c81b28
Muhammad Asad Summair <62187782+masads@users.noreply.github.com>
2021-10-23 03:30:24 +0500

    middlewares

--- server/server.js
+++ server/server.js
@@ -1,26 +1,33 @@
-
 const express = require('express');
+const {json , urlencoded} = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const port = 3000;
-var db = require('./utils/database.js');
+//db not setup yet
 
+// var db = require('./utils/database.js');
+// db.connect(function(err) {
+//   db.query("select * from testnode;" , function(err,result,fields)
+//   {
+//     setValue(result);
+//   });
+// });
+// function setValue(res)
+// {
+//   data=Object.values(JSON.parse(JSON.stringify(res)));
+// }
 
-db.connect(function(err) {
-  db.query("select * from testnode;" , function(err,result,fields)
-  {
-    setValue(result);
-  });
-});
+//middleware
+app.use(cors()); //making cross-domain requests possible  
+app.use(json());// tells the system that you want json to be used. 
+app.use(urlencoded({extended:true}));//allow us to attach parameters to a url like(?) etc
+app.use(morgan('dev'));//it does all the loging for us (run server by using "yarn dev" for "dev" detail got package.json scripts)
 
-console.log(data);
-function setValue(res)
-{
-  data=Object.values(JSON.parse(JSON.stringify(res)));
-}
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+//routing
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send({'message':'ok'});
 });
 
 app.listen(port, () => {
