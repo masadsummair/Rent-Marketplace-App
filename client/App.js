1300a3cb40e0ad50628322a9eda98a4e75abb531
Muhammad Asad Summair <62187782+masads@users.noreply.github.com>
2021-11-01 18:35:28 +0500

    add react-native-elements

--- client/App.js
+++ client/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Button  icon={    <Icon      name="arrow-right"      size={15}      color="white"    />  }  title="Button with icon component"/>
+      </View>
   );
 }
 
