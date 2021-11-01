192389b53b7d33798f2a982d92c5839e6cdd37a7
Muhammad Asad Summair <62187782+masads@users.noreply.github.com>
2021-11-01 17:43:43 +0500

    resolve error in client

--- client/App.js
+++ client/App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
