ee4bd3e1e6dd2ae2fe17011e56b10e73e6069981
Muhammad Asad Summair <62187782+masads@users.noreply.github.com>
2021-11-06 13:39:07 +0500

    test

---
+++ server/config/database.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const config = 
+{
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'dbproject'
+};
+const db =  mysql.createConnection(config);
+
+db.connect(err=>
+{
+    if(err)
+    {
+        throw err
+    }
+    console.log("my sql connected");
+});
+module.exports = db;
