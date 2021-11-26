4c581f60230453819d4f13d558a8ec8b21f8d3be
Uzair-Ali-Dev <uzali1000@gmail.com>
2021-11-27 03:14:22 +0500

    Changes in backend

--- server/config/database.js
+++ server/config/database.js
@@ -1,19 +1,16 @@
-const mysql = require('mysql');
-const config = 
-{
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'dbproject'
+const mysql = require("mysql");
+const config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "dbproject",
 };
-const db =  mysql.createConnection(config);
+const db = mysql.createConnection(config);
 
-db.connect(err=>
-{
-    if(err)
-    {
-        throw err
-    }
-    console.log("my sql connected");
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("my sql connected");
 });
 module.exports = db;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,27 +1,32 @@
-const express =  require('express');
+const express = require("express");
 
-const { signup,login,isAuth } = require('../controllers/auth.js');
+const { signup, login, isAuth } = require("../controllers/auth.js");
 
-const { viewItem,addItem,updateItem,deleteItem} = require('../controllers/item.js'); 
+const {
+  viewItem,
+  addItem,
+  updateItem,
+  deleteItem,
+} = require("../controllers/item.js");
 
 const router = express.Router();
 
-router.post('/login',login);
+router.post("/login", login);
 
-router.post('/signup',signup);
+router.post("/signup", signup);
 
-router.post('/additem',addItem);
+router.post("/additem", addItem);
 
-router.post('/deleteitem',deleteItem);
+router.post("/deleteitem", deleteItem);
 
-router.post('/updateitem',updateItem);
+router.post("/updateitem", updateItem);
 
-router.post('/viewitem',viewItem);
+router.post("/viewitem", viewItem);
 
-router.get('/private',isAuth);
+router.get("/private", isAuth);
 
-router.get('/public',(req,res,next)=>{
-    res.status(200).json({'message':"here is your public resoures"});
+router.get("/public", (req, res, next) => {
+  res.status(200).json({ message: "here is your public resoures" });
 });
 
 module.exports = router;
